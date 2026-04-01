8edfb3dca80158baa482a9022078b831a74aa17f
ray <sunlike0857@gmail.com>
2026-04-01 14:58:10 +0900

    利用規約を別タブで開けるようにした

--- js/layout/footer.js
+++ js/layout/footer.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     <footer class="footer">
       <div class="footer-inner">
         <a href="/">トップ</a>
-        <a href="/terms.html">利用規約</a>
+        <a href="/terms.html" target="_blank">利用規約</a>
         <a href="https://forms.gle/8w97cB2f3q8kF1Ut8" target="_blank">お問い合わせ</a>
         <a href="https://konohana-fusion.jp/" target="_blank">運営会社:コノハナ・フュージョン合同会社</a>
         <span class="footer-domain"><a href="https://mugendrill.com/">©2026 無限アプリ - mugendrill.com</a></span>
